8e59a8011f9b3034645fcf517e72a93e3342104b
FrancOlano <olanofranco1@gmail.com>
2022-05-20 12:38:45 +0200

    rutas index.js

--- react-mesa2/src/index.js
+++ react-mesa2/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter,
+  Routes,
+  Route,} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
   </BrowserRouter>
 );
 
